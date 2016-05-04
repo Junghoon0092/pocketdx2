1216999164f2f36d38513e583404d5288958fd0f
Junghoon <cjh0092@naver.com>
2016-05-05 06:48:12 +0900

    Initial Commit

---
+++ pocketmon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocketmon
+//
+//  Created by Junghoon on 2016. 5. 5..
+//  Copyright © 2016년 Junghoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
